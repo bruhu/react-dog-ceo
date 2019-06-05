ec53397531194fe599f086e282c516da5932a751
Bru <annabru.abh@gmail.com>
2019-06-05 11:25:56 +0200

    Created folder structure, wrote three action types for API call, start writing reducer in /reducer/index.js

---
+++ src/actions/actionTypes.js
@@ -0,0 +1,5 @@
+//this is where I store my actions - I write strings just one time, and I rely on constants that will be recognized in any environment -> helpful for hints and to avoid typos
+
+export const API_CALL_REQUEST = "API_CALL_REQUEST";
+export const API_CALL_SUCCESS = "API_CALL_SUCCESS";
+export const API_CALL_FAILURE = "API_CALL_FAILURE";
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+//here we start using Redux for real
+//import actions we defined:
+import {
+  API_CALL_REQUEST,
+  API_CALL_REQUEST,
+  API_CALL_FAILURE
+} from "../actions/actionTypes";
+
+//the reducer
