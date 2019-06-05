e7dde53d60a1c4234de5c9833686050e3228e54d
Bru <annabru.abh@gmail.com>
2019-06-05 13:38:26 +0200

    fixed more exports

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,7 +1,7 @@
-import { takeLatest } from "redux-saga/effects";
+import { takeLatest, call, put } from "redux-saga/effects";
 import axios from "axios";
 
-function* watcherSaga() {
+export function* watcherSaga() {
   yield takeLatest("API_CALL_REQUEST", workerSaga);
 }
 
@@ -17,8 +17,11 @@ function* workerSaga() {
   try {
     const response = yield call(fetchDog);
     const dog = response.data.message;
+
+    // dispatch a success action to the store with the new dog
     yield put({ type: "API_CALL_SUCCESS", dog });
   } catch (error) {
+    // dispatch a failure action to the store with the error
     yield put({ type: "API_CALL_FAILURE", error });
   }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,19 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { reducer } from "../reducers";
+import createSagaMiddleware from "redux-saga";
+import { watcherSaga } from "../sagas";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const devTools =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
-export const store = createStore(reducer, devTools);
+//method from Redux to use Saga:
+const composed = compose(
+  applyMiddleware(sagaMiddleware),
+  devTools
+);
+
+export const store = createStore(reducer, composed);
+
+sagaMiddleware.run(watcherSaga);
