aa37591d32ff58cd0a0eee0fcadab9f6c4016358
Bru <annabru.abh@gmail.com>
2019-06-05 11:33:47 +0200

    Writing Reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,27 @@
 import {
   API_CALL_REQUEST,
   API_CALL_REQUEST,
-  API_CALL_FAILURE
+  API_CALL_FAILURE,
+  API_CALL_SUCCESS
 } from "../actions/actionTypes";
 
-//the reducer
+//define an initial state
+const initialState = {
+  dog: null,
+  fetching: false,
+  error: false
+};
+
+//the reducer - we will use the constants here to avoid typos
+function reducer(state, action) {
+  switch (action.type) {
+    case API_CALL_REQUEST:
+      return { ...state, fetching: true, error: false };
+    case API_CALL_SUCCESS:
+      return;
+    case API_CALL_FAILURE:
+      return;
+    default:
+      return;
+  }
+}
